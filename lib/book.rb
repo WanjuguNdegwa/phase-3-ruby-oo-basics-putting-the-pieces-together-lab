31765e9a4ed367ba7055a490013310d8e6f70378
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-06-13 12:59:39 +0300

    commit tree 2134c203600d34feab3f89c6af6af376bf741444

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+  
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
+  def initialize(title = "And Then There Were None")
+    @title = title
+  end
 
+    def turn_page 
+      puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+book = Book.new
+book.title
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
